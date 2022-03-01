826dbe3caaa027e31de02713270898f940fad1eb
LORD-Zenix <nandananda@gmail.com>
2022-03-01 13:56:23 +0700

    AlphabotV16

---
+++ shop/allpayment.js
@@ -0,0 +1,34 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ZEEONE OFC
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0887 4350 47326
+2. BCA 
+	ㅁ 3901 0887 4350 47326
+3. BNI 
+	ㅁ 881 0887 4350 47326
+4. PERMATA
+	ㅁ 852 8887 4350 47326
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0857 0186 3281
+2. OVO
+	ㅁ 0857 0186 3281
+3. DANA
+	ㅁ 0857 0186 3281
+	
+Semuanya atas nama N***A
+
+*📲 PULSA*
+    ㅁ 0857 0186 3281
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0887 4350 47326
+2. BCA 
+	ㅁ 3901 0887 4350 47326
+3. BNI 
+	ㅁ 881 0887 4350 47326
+4. PERMATA
+	ㅁ 852 8887 4350 47326
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0857 0186 3281
+2. PULSA
+	ㅁ 0857 0186 3281
+3. DANA
+	ㅁ 0857 0186 3281
+	
+Semuanya atas nama N***A
+`
+	}
+
+exports.donasibot = donasibot
+ 
